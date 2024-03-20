765310ef5cb0282d70647a500afbe0f1c766dc2f
burakenes04 <164189666+burakenes04@users.noreply.github.com>
2024-03-20 16:15:40 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,3 @@
+// Background Carousel
+/*example - bgCarousel(img directory - string,images -array,slide duration - number, default:5000,animation transition - string, default:1s)*/
+bgCarousel("img/backgrounds",["bg-1.jpg","bg-2.jpg","bg-3.jpg","bg-4.jpg","bg-5.jpg"],6000,"2s")
